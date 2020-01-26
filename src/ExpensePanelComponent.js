bc8d3592bcff4dd62048914b11f8d3330ec3a023
Dong Thien Dao <dong@Dongs-MacBook-Pro.local>
2020-01-26 18:11:22 +0900

    add list

--- src/ExpensePanelComponent.js
+++ src/ExpensePanelComponent.js
@@ -5,14 +5,14 @@ import { Table } from "react-bootstrap";
 const ExpensePanelComponent = params => {
   return (
     <div className="container">
-      <div className="row">
+      {/* <div className="row">
         <div
           className="col col-xs-8 offset-xs-2"
           style={{ backgroundColor: "rgb(245,245,245)" }}
         >
           <span>{moment().format("LL")}</span>
         </div>
-        <Table responsive>
+        <Table responsive variant="dark">
           <tbody>
             <tr>
               <td>Table cell</td>
@@ -34,78 +34,54 @@ const ExpensePanelComponent = params => {
             </tr>
           </tbody>
         </Table>
-      </div>
-      <div className="row">
-        <div
-          className="col col-xs-8 offset-xs-2"
-          style={{ backgroundColor: "rgb(245,245,245)" }}
-        >
-          <span>{moment().format("LL")}</span>
-        </div>
-        <Table responsive>
-          <tbody>
-            <tr>
-              <td>Table cell</td>
-              <td>Table cell</td>
-              <td>Table cell</td>
-              <td>Table cell</td>
-            </tr>
-            <tr>
-              <td>Table cell</td>
-              <td>Table cell</td>
-              <td>Table cell</td>
-              <td>Table cell</td>
-            </tr>
-            <tr>
-              <td>Table cell</td>
-              <td>Table cell</td>
-              <td>Table cell</td>
-              <td>Table cell</td>
-            </tr>
-          </tbody>
-        </Table>
-      </div>
-      <div className="row">
-        <div
-          className="col col-xs-8 offset-xs-2"
-          style={{ backgroundColor: "rgb(245,245,245)" }}
-        >
-          <span>{moment().format("LL")}</span>
-        </div>
-        <Table responsive>
-          <tbody>
-            <tr>
-              <td>Table cell</td>
-              <td>Table cell</td>
-              <td>Table cell</td>
-              <td>Table cell</td>
-            </tr>
-            <tr>
-              <td>Table cell</td>
-              <td>Table cell</td>
-              <td>Table cell</td>
-              <td>Table cell</td>
-            </tr>
-            <tr>
-              <td>Table cell</td>
-              <td>Table cell</td>
-              <td>Table cell</td>
-              <td>Table cell</td>
-            </tr>
-            <tr>
-              <td>Table cell</td>
-              <td>Table cell</td>
-              <td>Table cell</td>
-              <td>Table cell</td>
-            </tr>
-            <tr>
-              <td>Table cell</td>
-              <td>Table cell</td>
-              <td>Table cell</td>
-              <td>Table cell</td>
-            </tr>
-          </tbody>
-        </Table>
+      </div> */}
+      <table class="table table-condensed table-bordered">
+        <caption>List of stuff</caption>
+        <thead>
+          <tr>
+            <th scope="col">#</th>
+            <th scope="col">First</th>
+            <th scope="col">Last</th>
+            <th scope="col">Handle</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <th scope="row">1</th>
+            <td>Mark</td>
+            <td>Otto</td>
+            <td>@mdo</td>
+          </tr>
+          <tr>
+            <th scope="row">2</th>
+            <td>Jacob</td>
+            <td>Thornton</td>
+            <td>@fat</td>
+          </tr>
+          <tr>
+            <th scope="row">3</th>
+            <td>Larry</td>
+            <td>the Bird</td>
+            <td>@twitter</td>
+          </tr>
+        </tbody>
+      </table>
+      <div class="list-group">
+        <button type="button" class="list-group-item">
+          Cras justo odio
+        </button>
+        <button type="button" class="list-group-item">
+          Dapibus ac facilisis in
+        </button>
+        <button type="button" class="list-group-item">
+          Morbi leo risus
+        </button>
+        <button type="button" class="list-group-item">
+          Porta ac consectetur ac
+        </button>
+        <button type="button" class="list-group-item">
+          Vestibulum at eros
+        </button>
       </div>
     </div>
   );
